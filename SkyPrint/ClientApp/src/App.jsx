7000d663f9fee640024157531264b14d970ce46a
Ivaschenko Igor <ivaschenko.i.y@gmail.com>
2018-11-09 19:57:36 +0600

    create header and footer

---
+++ SkyPrint/ClientApp/src/App.jsx
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import { Route } from 'react-router';
+import Header from './components/headerComponent';
+import Footer from './components/footerComponent';
+
+export default () => (
+    <React.Fragment>
+        <Header />
+        <main>
+            fsdfsdf
+        </main>
+        <Footer />
+    </React.Fragment>
+);
---
+++ SkyPrint/ClientApp/src/components/footerComponent.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { actionCreators } from '../store/Counter';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="footer-wrap">
+        <div className="wrapper">
+          <div className="footer-col footer-copyright idesc">
+            <p>© SkyPrint, 2018<br />
+              Полиграфические услуги.
+              </p>
+          </div>
+
+          <div className="footer-col">
+            <ul id="footer-nav-list" className="footer-nav-list"><li><a href="http://499363.ru/o-kompanii/">О компании</a></li>
+              <li><a href="http://499363.ru/oplata-i-dostavka/">Оплата и доставка</a></li>
+              <li><a href="http://499363.ru/trebovaniya-k-maketam/">Требования к макетам</a></li>
+              <li><a href="http://499363.ru/kontakty/">Контакты</a></li>
+            </ul>                </div>
+
+          <div className="footer-col footer-contacts idesc">
+            <p>E-mail: <a href="mailto:567@499363.ru">567@499363.ru</a></p>
+            <p>г. Омск, ул. Ильинская 4, офис 61</p>
+            <p><a href="#box-map-go" data-fancybox="">Карта проезда</a></p>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default connect(
+  state => state.counter,
+  dispatch => bindActionCreators(actionCreators, dispatch)
+)(Footer);
