1b90f0106f753595733fbb9d43b91f42ea5a4dc0
Ivaschenko Igor <ivaschenko.i.y@gmail.com>
2018-11-12 19:48:52 +0600

    edit alert, edit form comments, edit services

---
+++ SkyPrint/ClientApp/src/components/orderNotFoundComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function OrderNotFound(props) {
+  return (
+    <div className="not_found_page">
+      <h1 className="number_order">Данный заказ не найден</h1>
+    </div>
+  )
+}
--- SkyPrint/ClientApp/src/service/httpService.js
+++ SkyPrint/ClientApp/src/service/httpService.js
@@ -3,3 +3,7 @@ import axios from 'axios';
 export function httpGet(url) {
   return axios.get(url);
 }
+
+export function httpPost(url, data) {
+  return axios.post(url, data);
+}
