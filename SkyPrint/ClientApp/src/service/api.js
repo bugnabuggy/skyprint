0ddd9fa3e3827ae6431ebc256ceed6891b3c5c92
Anton Yakovets <jokerosky@gmail.com>
2018-12-19 12:56:50 +0600

    self contained windows service version

--- SkyPrint/ClientApp/src/service/api.js
+++ SkyPrint/ClientApp/src/service/api.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'http://sp.ruteco.com';
+export const baseUrl = 'http://zakaz.499363.ru'; //'http://sp.ruteco.com';
 export const orderDataUrl = `${baseUrl}/api/order/%id%`;
--- SkyPrint/Program.cs
+++ SkyPrint/Program.cs
@@ -1,10 +1,12 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -14,11 +16,21 @@ namespace SkyPrint
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args)
+                .UseUrls("http://0.0.0.0:80/") // ← remove this for development
+                .Build();
+
+            host.RunAsService();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var pathToExe = Process.GetCurrentProcess().MainModule.FileName;  // ← remove this for development
+            var pathToContentRoot = Path.GetDirectoryName(pathToExe); // ← remove this for development
+
+            return WebHost.CreateDefaultBuilder(args)
+                .UseContentRoot(pathToContentRoot)  // ← remove this for development
                 .UseStartup<Startup>();
+        }
     }
 }
